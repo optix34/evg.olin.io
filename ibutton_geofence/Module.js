9547163e12c96d37953d291b678bae75b045d02f
evg <optix500@gmail.com>
2026-05-29 11:55:18 +0300

    Update Module.js

--- ibutton_geofence/Module.js
+++ ibutton_geofence/Module.js
@@ -3,9 +3,25 @@ Ext.define('Store.test_ble.Module', {
     singleton: true,
 
     initModule: function() {
-        return Ext.create('Ext.panel.Panel', {
+        var mainPanel = Ext.create('Ext.panel.Panel', {
             title: 'Test',
             html: 'OK'
         });
+
+        var navTab = Ext.create('Ext.panel.Panel', {
+            title: 'Test',
+            iconCls: 'fa fa-car',
+            map_frame: mainPanel
+        });
+
+        if (window.skeleton && window.skeleton.navigation) {
+            window.skeleton.navigation.add(navTab);
+        }
+
+        if (window.skeleton && window.skeleton.mapframe) {
+            window.skeleton.mapframe.add(mainPanel);
+        }
+
+        return mainPanel;
     }
 });
