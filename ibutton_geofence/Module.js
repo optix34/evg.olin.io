d228fd2fef31c4ff4478562c4f9f7eaaa7d5aa45
evg <optix500@gmail.com>
2026-05-29 12:34:54 +0300

    Update Module.js

--- ibutton_geofence/Module.js
+++ ibutton_geofence/Module.js
@@ -1,28 +1,16 @@
 Ext.define('Store.test_ble.Module', {
-    extend: 'Ext.Component',
     singleton: true,
-
     initModule: function() {
-        var mainPanel = Ext.create('Ext.panel.Panel', {
-            title: 'Тестовое расширение',
-            html: '<h2>Успех!</h2><p>Расширение test_ble работает.</p>',
-            bodyPadding: 10
+        var panel = Ext.create('Ext.panel.Panel', {
+            title: 'Test',
+            html: 'OK'
         });
-
-        var navTab = Ext.create('Ext.panel.Panel', {
-            title: 'Test BLE',
-            iconCls: 'fa fa-bluetooth',
-            layout: 'fit',
-            items: [mainPanel],
-            map_frame: mainPanel
-        });
-
         if (window.skeleton && window.skeleton.navigation) {
-            window.skeleton.navigation.add(navTab);
-        } else {
-            console.warn('test_ble: skeleton.navigation не найден');
+            window.skeleton.navigation.add({
+                title: 'Test',
+                map_frame: panel
+            });
         }
-
-        return mainPanel;
+        return panel;
     }
 });
